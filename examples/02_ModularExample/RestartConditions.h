57c3f9ec8e1ee4a7b1d5c61101d5817bda847bb9
artkeller <artkeller@gmx.de>
2026-08-09 23:52:06 +0200

    Add RestartConditions.h for modular restart handling

---
+++ examples/02_ModularExample/RestartConditions.h
@@ -0,0 +1,58 @@
+/**
+ * @file RestartConditions.h
+ * @brief Defines modular restart conditions and callbacks for ESP32State.
+ */
+
+#ifndef RESTARTCONDITIONS_H
+#define RESTARTCONDITIONS_H
+
+#include <ESP32State.h>
+#include <esp_system.h>
+#include <esp_sleep.h>
+#include <vector>
+
+/**
+ * @brief Retrieves a vector of restart conditions and their corresponding callback functions.
+ * @note Das 'inline' Keyword verhindert Linker-Konflikte beim mehrfachen Einbinden!
+ */
+inline std::vector<ESP32State::ConditionPair> getRestartConditions() {
+    return {
+        // Condition 1: Panic reset
+        {
+            []() { return esp_reset_reason() == ESP_RST_PANIC; },
+            []() {
+                ESP32STATE_LOG(ESP32State::LogLevel::ERROR, "Panic reset detected! Unhandled exception occurred.");
+                ESP32STATE_LOG(ESP32State::LogLevel::ERROR, "System halting for safety.");
+                while (true) {
+                    delay(100);
+                }
+            }
+        },
+
+        // Condition 2: Unknown reset reason
+        {
+            []() { return esp_reset_reason() == ESP_RST_UNKNOWN; },
+            []() {
+                ESP32STATE_LOG(ESP32State::LogLevel::WARN, "Reset reason could not be determined. Possible firmware update.");
+            }
+        },
+
+        // Condition 3: Power-on reset
+        {
+            []() { return esp_reset_reason() == ESP_RST_POWERON; },
+            []() {
+                ESP32STATE_LOG(ESP32State::LogLevel::INFO, "Power-on reset detected.");
+            }
+        },
+
+        // Condition 4: Wakeup by external signal (EXT0)
+        {
+            []() { return esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT0; },
+            []() {
+                ESP32STATE_LOG(ESP32State::LogLevel::INFO, "Wakeup by external signal (EXT0) detected.");
+            }
+        }
+    };
+}
+
+#endif // RESTARTCONDITIONS_H
