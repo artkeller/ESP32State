fe9007cf733d23e8c6acf0ec961d845b392fffb1
artkeller <artkeller@gmx.de>
2026-08-10 00:03:07 +0200

    Fix formatting and update external signal wakeup condition

--- examples/03_AdvancedConditions/RestartConditions.h
+++ examples/03_AdvancedConditions/RestartConditions.h
@@ -34,9 +34,8 @@ inline std::vector<ESP32State::ConditionPair> getRestartConditions() {
             []() { ESP32STATE_LOG(ESP32State::LogLevel::INFO, "Power-on detected."); }
         },
         
-
-#if SOC_PM_SUPPORT_EXT0_WAKEUP
         // Condition 4: External Signal Wakeup (EXT0)
+#if SOC_PM_SUPPORT_EXT0_WAKEUP
         {
             []() { return esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT0; },
             []() { ESP32STATE_LOG(ESP32State::LogLevel::INFO, "Wakeup by external signal detected."); }
