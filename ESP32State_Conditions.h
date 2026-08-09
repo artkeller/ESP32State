c4fa918673d46568ad56d1c1e72fed261ac5e84b
artkeller <artkeller@gmx.de>
2026-08-09 23:09:42 +0200

    Refactor reset and wakeup condition handling

--- ESP32State_Conditions.h
+++ ESP32State_Conditions.h
@@ -1,63 +1,85 @@
 /**
  * @file ESP32State_Conditions.h
- * @brief Flexible default conditions for ESP32 reset and wakeup causes.
- * @author Thomas Walloschke <artkeller@gmc.de>
+ * @brief Predefined conditions for ESP32 reset and wakeup reasons.
  */
 
 #ifndef ESP32STATE_CONDITIONS_H
 #define ESP32STATE_CONDITIONS_H
 
-#include <ESP32State.h>
-#include "ESP32StateConfig.h"
+#include "ESP32State.h"
 #include <esp_system.h>
 #include <esp_sleep.h>
 #include "soc/soc_caps.h"
 
 namespace ESP32StateDefaults {
 
-inline std::vector<ESP32State::Condition> getResetConditions() {
-    return {
-        {[]() { return esp_reset_reason() == ESP_RST_UNKNOWN; }, 
-         []() { ESP32State::log(ESP32State::LogLevel::INFO, "Unknown reset detected."); }},
-        
-        {[]() { return esp_reset_reason() == ESP_RST_POWERON; }, 
-         []() { ESP32State::log(ESP32State::LogLevel::INFO, "Power-on reset detected."); }},
+inline std::vector<ESP32State::ConditionPair> getResetConditions() {
+    std::vector<ESP32State::ConditionPair> conditions;
 
-        {[]() { return esp_reset_reason() == ESP_RST_PANIC; }, 
-         []() { ESP32State::log(ESP32State::LogLevel::ERROR, "CRITICAL: Panic reset detected!"); }},
-        
-        {[]() { return esp_reset_reason() == ESP_RST_BROWNOUT; }, 
-         []() { ESP32State::log(ESP32State::LogLevel::ERROR, "WARNING: Brownout reset detected!"); }},
-        
-        {[]() { return esp_reset_reason() == ESP_RST_DEEPSLEEP; }, 
-         []() { ESP32State::log(ESP32State::LogLevel::INFO, "Deep sleep wake-up reset detected."); }}
-    };
+    // Universelle Reset-Gründe
+    conditions.push_back({
+        []() { return esp_reset_reason() == ESP_RST_POWERON; },
+        []() { ESP32STATE_LOG(ESP32State::LogLevel::INFO, "Power-on reset."); }
+    });
+
+    conditions.push_back({
+        []() { return esp_reset_reason() == ESP_RST_PANIC; },
+        []() { ESP32STATE_LOG(ESP32State::LogLevel::ERROR, "CRITICAL: Panic reset!"); }
+    });
+
+    conditions.push_back({
+        []() { return esp_reset_reason() == ESP_RST_BROWNOUT; },
+        []() { ESP32STATE_LOG(ESP32State::LogLevel::ERROR, "WARNING: Brownout reset!"); }
+    });
+
+    conditions.push_back({
+        []() { return esp_reset_reason() == ESP_RST_DEEPSLEEP; },
+        []() { ESP32STATE_LOG(ESP32State::LogLevel::INFO, "Deep sleep wake reset."); }
+    });
+
+    // Hardware-Geguardet (Wird bei Modellen ohne entsprechendes Feature nicht kompiliert)
+#if defined(ESP_RST_USB)
+    conditions.push_back({
+        []() { return esp_reset_reason() == ESP_RST_USB; },
+        []() { ESP32STATE_LOG(ESP32State::LogLevel::INFO, "USB peripheral reset."); }
+    });
+#endif
+
+    return conditions;
 }
 
-inline std::vector<ESP32State::Condition> getWakeupConditions() {
-    return {
-        {[]() { return esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_TIMER; }, 
-         []() { ESP32State::log(ESP32State::LogLevel::INFO, "Wakeup caused by timer."); }},
+inline std::vector<ESP32State::ConditionPair> getWakeupConditions() {
+    std::vector<ESP32State::ConditionPair> conditions;
+
+    conditions.push_back({
+        []() { return esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_TIMER; },
+        []() { ESP32STATE_LOG(ESP32State::LogLevel::INFO, "Wakeup caused by timer."); }
+    });
 
 #if SOC_TOUCH_SUPPORTED
-        {[]() { return esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_TOUCHPAD; }, 
-         []() { ESP32State::log(ESP32State::LogLevel::INFO, "Wakeup caused by touchpad interrupt."); }},
+    conditions.push_back({
+        []() { return esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_TOUCHPAD; },
+        []() { ESP32STATE_LOG(ESP32State::LogLevel::INFO, "Wakeup caused by touchpad."); }
+    });
 #endif
 
 #if SOC_PM_SUPPORT_EXT0_WAKEUP
-        {[]() { return esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT0; }, 
-         []() { ESP32State::log(ESP32State::LogLevel::INFO, "Wakeup caused by external signal (EXT0)."); }},
+    conditions.push_back({
+        []() { return esp_sleep_get_wakeup_cause() == ESP_SLEEP_WAKEUP_EXT0; },
+        []() { ESP32STATE_LOG(ESP32State::LogLevel::INFO, "Wakeup caused by EXT0 signal."); }
+    });
 #endif
-    };
+
+    return conditions;
 }
 
-inline void runDefaultDiagnostics() {
+inline ESP32State::AnalysisResult runDefaultDiagnostics() {
     auto resets = getResetConditions();
     auto wakeups = getWakeupConditions();
     resets.insert(resets.end(), wakeups.begin(), wakeups.end());
 
     ESP32State manager(resets);
-    manager.analyze();
+    return manager.analyze();
 }
 
 } // namespace ESP32StateDefaults
